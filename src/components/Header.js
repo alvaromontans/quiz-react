a74c59cea8a7fa244141be048fd8c0b56a366a16
alvaromon <alvaromifp@gmail.com>
2025-03-22 22:40:25 +0100

    Actualizacion header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
+import { useQuiz } from "../hooks/useQuiz";
+
 function Header() {
+  const { dispatch } = useQuiz();
+
   return (
-    <header className='app-header'>
+    <header className='app-header' onClick={() => dispatch({ type: "restart" })}>
       <img src='logo512.png' alt='React logo' />
       <h1>Quiz React</h1>
     </header>
